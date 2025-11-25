d7ed0c18a102e3d6a375eecc931b520576052a31
Saraqch <quispechoquesara@gmail.com>
2025-11-25 06:59:09 -0400

    fix: index deploy

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import "dotenv/config";
 import Server from "./config/server.config";
 import { SERVER_PORT } from "./config/env.config";
 
+export default Server; 
+
 async function startServer() {
   try {
     Server.listen(SERVER_PORT, () => {
