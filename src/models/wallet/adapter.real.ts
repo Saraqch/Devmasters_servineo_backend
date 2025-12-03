233ae600f65f12662f3c8b1122667616378caf2c
johnivansn <johnivansn@gmail.com>
2025-12-02 20:37:17 -0400

    fix: comentar la importación de dotenv en adapter.real.ts

--- src/models/wallet/adapter.real.ts
+++ src/models/wallet/adapter.real.ts
@@ -1,4 +1,4 @@
-import 'dotenv/config';
+/*import 'dotenv/config';
 import { makeRawCollectionWalletAdapter } from './adapter';
 import { makeWalletCollectionByUserIdAdapter } from './adapter.collection';
 
@@ -17,3 +17,4 @@ export function resolveWalletAdapter() {
 }
 
 export const walletAdapterReal = resolveWalletAdapter();
+*/
